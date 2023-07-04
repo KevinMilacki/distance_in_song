1c9be0c9bad8a72ab90b47e2799bf096d68e98d1
Kevin Milacki <kevin.milacki@gmail.com>
2023-07-04 12:00:49 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 
 function App() {
+ 
+  // const [token, setToken] = useState('');
+
+  useEffect(() => {
+    fetch('localhost:3001/api/test')
+    .then(response => {
+      response.json()
+      .then(json => {
+        console.log(json)
+      });
+    });
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+      <Container>
+        <Row>
+          <Col><h1>Distance in Song</h1></Col>
+        </Row> 
+        <Row>
+        <Col>
+         <Card className="custom-spotify">
+          <Card.Body>
+            <Card.Title>Spotify Side</Card.Title>
+            <Form>
+      <Form.Group className="mb-3" controlId="formSongTitle">
+        <Form.Label>Song Title</Form.Label>
+        <Form.Control type="text" placeholder="Song Title" />
+        <Form.Text className="text-muted">
+          Enter the Song of your Choice
+        </Form.Text>
+      </Form.Group>
+
+     
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+          </Card.Body>
+         </Card>
+        </Col>
+        <Col>
+         <Card className='custom-google'>
+          <Card.Body>
+            <Card.Title>Google Maps Side</Card.Title>
+            <Form>
+      <Form.Group className="mb-3" controlId="formOrigin">
+        <Form.Label>Origin</Form.Label>
+        <Form.Control type="text" placeholder="Origin" />
+        <Form.Text className="text-muted">
+          Enter Starting Point
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formDestination">
+        <Form.Label>Destination</Form.Label>
+        <Form.Control type="text" placeholder="Destination" />
+        <Form.Text className="text-muted">
+          Enter Ending Point
+        </Form.Text>
+      </Form.Group>
+
+     
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+          </Card.Body>
+         </Card>
+        </Col> 
+        </Row>
+     
+      </Container>
+
+    </>
   );
 }
 
